23c5b56818f98a94248f3b695519cb63ad74a156
Jesse Stricker <me@jessestricker.de>
2016-02-23 13:23:13 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 # install cargo
-function install_mdbook
-{
-    if [ -f "$WERCKER_CACHE_DIR/cargo/mdbook" ]; then
-        debug "mdbook found in cache"
-    else
-        debug "downloading mdbook"
-        cargo install mdbook
-        mkdir -p "$WERCKER_CACHE_DIR/cargo"
-        cp "~/.cargo/bin/mdbook" "$WERCKER_CACHE_DIR/cargo/mdbook"
-    fi
-}
+if [ -f "$WERCKER_CACHE_DIR/cargo/mdbook" ]; then
+    echo "mdbook found in cache"
+else
+    echo "downloading mdbook"
+    cargo install mdbook
+    mkdir -p "$WERCKER_CACHE_DIR/cargo"
+    cp "~/.cargo/bin/mdbook" "$WERCKER_CACHE_DIR/cargo/mdbook"
+fi
 
-function build
-{
-    debug "building the book"
-    "$WERCKER_CACHE_DIR/cargo/mdbook" build
-}
-
-install_mdbook
-build
+echo "building the book"
+"$WERCKER_CACHE_DIR/cargo/mdbook" build
